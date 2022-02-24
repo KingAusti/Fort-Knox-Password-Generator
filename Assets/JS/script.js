3ece1051a52a8f55edeb2d18d143ec9a1cca63f8
KingAusti <austin@thehenrys.xyz>
2022-02-23 20:37:48 -0800

    more work = more bugs. Still working them out.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,4 @@
 // Assignment code below
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,31 +9,29 @@ var charray = [
   ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
   [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
 ];
-console.log(charray)
+//console.log(charray)
 
 function pwCreation() {
-  var pwCompile = pwBuilder();
+  var pwBuild = pwBuilder();
   var pwDom = document.querySelector("#password");
-  pwDom.value = pwCompile;
+  pwDom.value = pwBuild;
 
     function pwBuilder() {
       var usrSelection = [];
-
-      pwStructure(charray, usrSelection, finalResult);
-      var finalResult = "";
+      
+      pwStructure(charray, usrSelection, newPw);
+      var newPw = "";
       if(usrSelection.length !== 0) {
-        var finalResult = retrievePwLength();
-        for(var i = 0; i < finalResult; i++){
-          finalResult += passwordType[Math.floor(Math.random() * usrSelection.length)];
+        var pwLength = retrievePwLength();
+        for(var i = 0; i < pwLength; i++){
+          newPw += usrSelection[Math.floor(Math.random() * usrSelection.length)];
         }
       }
-      return finalResult
+      return newPw;
     }
 }
-
-
 //user prompts for password parameters
-function pwStructure( userPrompt, allInfo){
+function pwStructure(userPrompt, allInfo, newPwPrompt){
   //window prompts output to user
   //prompt for lowercase characters
   var lwrCase = window.confirm("Would you like LOWERCASE characters? Okay for YES, Cancel for NO.");
@@ -43,72 +40,60 @@ function pwStructure( userPrompt, allInfo){
   //prompt for special characters
   var spcCase = window.confirm("Would you like SPECIAL characters? Okay for YES, Cancel for NO.");
   //prompt for numbers
-  var nmCase = window.confirm("Would you like NUMERIC characters? Okay for YES, Cancel for NO.");
-  //final if just in case the user is cheeky and selects NO to all prompts
-  // if (userPrompt.length === false) {
-  //   window.alert("ERROR:You must select YES on at least one prompt");
-  //   pwStructure( userPrompt, allInfo);
-  // };
-  //console.log(lwrCase)
+  var nmCase = window.confirm("Would you like NUMERIC characters? Okay for YES, Cancel for NO.");  
+  
+  debugger
   //if user wants lowercase add  to array
   if (lwrCase === true){
-    userPrompt.push(...allInfo[0].slice(0))      
-  };
-  // console.log(lwrCase)
+    newPwPrompt = userPrompt.push(...allInfo[0].slice(0));
+  }
   
-  //console.log(upCase)
+  
+  
   //if user wants uppercase add to array
   if (upCase === true){
-    userPrompt.push(...allInfo[1].slice(0))
-  };
+    newPwPrompt = userPrompt.push(...allInfo[1].slice(0));
+  }
   
-  //console.log(spcCase)
+  
   //if user wants special add to array
   if (spcCase === true) {
-    userPrompt.push(...allinfo[2].slice(0))
-  };
+    newPwPrompt = userPrompt.push(...allInfo[2].slice(0));
+  }
   
-  //console.log(nmCase)
+  
   //if user wants number add to array
   if (nmCase === true) {
-    userPrompt.push(...allinfo[3].slice(0))
-  };
+    newPwPrompt = userPrompt.push(...allInfo[3].slice(0));
+  }
   
-};
-console.log(pwStructure());
+  //final if just in case the user is cheeky and selects NO to all prompts
+  if (userPrompt.length === 0) {
+    window.alert("<read in germanic accent>ACHTUNG:YOU MUST SELECT YES ON AT LEAST ONE PROMPT");
+    pwStructure(userPrompt, allInfo, newPwPrompt);
+  }
+}
 
-//debugger
+
+
 //prompt for desired password length
 function retrievePwLength() {
   pwLength = prompt("How long would you like your password to be?", "Min 8 char, Max 128 char.");
   if(pwLength !== null){
     parseInt(pwLength);
     //if statement for password not meeting requirement
-    if(isNaN(passwordLength) || (passwordLength < 8 || passwordLength > 128)){
-      window.alert('ERROR: Length does not meet requirement; Must be 8-128char in length')
-      getPasswordLength();
+    if(isNaN(pwLength) || (pwLength < 8 || pwLength > 128)){
+      window.alert('ERROR: Length does not meet requirement; PW must be 8-128char in length')
+      retrievePwLength();
     }
-    return passwordLength;
+    return pwLength;
   } else {
     return;
   }
-}
-  
-
-
-
-
-
-
-function writePassword() {
-  var password = generatePw();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-  return password;  
-    }
+};
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", pwCreation);
 
 
 //abandoning randomizer as most everyone I am talking to is using an array of characters. Having a lot of trouble outputting the generated data into anything readable 
@@ -116,6 +101,14 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // putting old code here as a reminder of past stuggles so I can convince myself to not give up.
+
+// function writePassword() {
+//   var password = generatePw();
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+//   return password;  
+//     }
+
 //prompt for length question to user
 // function = retrieve(pwPrompt, userChar); {
 //   
